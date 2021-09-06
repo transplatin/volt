12791000cba9cd429f5ac0d1b59d98eade456ea3
Vipin Kumar <Vk321901@gmail.com>
2021-09-06 22:17:15 +0530

    Version - 1.0.0
    Basic Setup With Theme

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
-import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createDrawerNavigator } from '@react-navigation/drawer'
+import Home from './screens/Home'
+import { Dimensions } from 'react-native'
+import Theme from './components/Theme'
+
+const Drawer = createDrawerNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer theme={Theme}>
+      <Drawer.Navigator
+        screenOptions={{
+          drawerType:
+            Dimensions.get('window').width > 786 ? 'permanent' : 'slide',
+        }}
+        initialRouteName="Home">
+        <Drawer.Screen name="Home" component={Home} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
---
+++ components/Theme.js
@@ -0,0 +1,20 @@
+const Theme = {
+  dark: false,
+  colors: {
+    round: 18,
+    elevation: 5,
+    fontSize: 15,
+    borderWidth: 0,
+    headerFontSize: 18,
+    borderColor: '#122C34',
+    primary: '#01A7C2',
+    danger: '#C91F1F',
+    success: '#31D0AA',
+    warning: '#F0803C',
+    background: '#ffffff',
+    text: '#122C34',
+    textDisabled: '#666171',
+  },
+}
+
+export default Theme
